a2b1f607f213a1fdd6cccd4a7331aa238a7d5bd5
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-08 02:07:47 +0330

    create data statice inpute done

---
+++ src/Data/DataInput.js
@@ -0,0 +1,7 @@
+const dataInputs = [
+  { id: 1, type: "text", name: "name", placeholder: "Plase Enter Name" },
+  { id: 2, type: "text", name: "lastName", placeholder: "Plase Enter LastName" },
+  { id: 3, type: "email", name: "email", placeholder: "Plase Enter Email" },
+  { id: 4, type: "number", name: "phone", placeholder: "Plase Enter Phone" },
+];
+export default dataInputs
