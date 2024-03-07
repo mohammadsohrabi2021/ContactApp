efe92fa2a701c27c6724fd531504687038bd8f34
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-08 02:04:46 +0330

    refactor component App done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Contact from "./Component/Contact"
+import Header from "./Component/Header"
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+     <Contact/>
+    </>
+  )
+}
+
+export default App
